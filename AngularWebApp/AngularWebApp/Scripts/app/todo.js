dd62a2d949fa9be1687b91a1463b24c748d93a64
vambak <vambak@live.com>
2015-08-08 14:51:59 +0100

    Todo app

--- AngularWebApp/AngularWebApp/Scripts/app/todo.js
+++ AngularWebApp/AngularWebApp/Scripts/app/todo.js
@@ -1,4 +1,25 @@
-﻿function TodoCtrl($scope) {
-    $scope.totalTodos = 5;
-    $scope.name = "";
-}
+﻿//function TodoCtrl($scope) {
+angular
+.module("app", [])
+.controller("TodoCtrl", function ($scope) {
+
+    $scope.todos = [
+        { text: "Learn angular", done: false },
+        { text: "learn more", done: false },
+    ];
+
+    $scope.getTotalTodos = function () {
+        return $scope.todos.length;
+    };
+
+    $scope.clearCompleted = function () {
+        $scope.todos = _.filter($scope.todos, function(todo){
+            return !todo.done;
+        });
+    };
+
+    $scope.addTodo = function () {
+        $scope.todos.push({ text: $scope.formTodoText, done: false });
+        $scope.formTodoText = '';
+    };
+});
